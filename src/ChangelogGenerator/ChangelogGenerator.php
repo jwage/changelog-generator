60f133733600de999c460076a3f4ce2b2023e14f
Jonathan H. Wage <jonwage@gmail.com>
2018-05-19 20:04:52 +0100

    Add total contributors to changelog.

--- src/ChangelogGenerator/ChangelogGenerator.php
+++ src/ChangelogGenerator/ChangelogGenerator.php
@@ -6,6 +6,8 @@ namespace ChangelogGenerator;
 
 use Symfony\Component\Console\Output\OutputInterface;
 use function array_filter;
+use function array_map;
+use function array_unique;
 use function count;
 use function sprintf;
 
@@ -33,6 +35,7 @@ class ChangelogGenerator
             '',
             sprintf('Total issues resolved: **%s**', $this->getNumberOfIssues($issues)),
             sprintf('Total pull requests resolved: **%s**', $this->getNumberOfPullRequests($issues)),
+            sprintf('Total contributors: **%s**', $this->getNumberOfContributors($issues)),
         ]);
 
         foreach ($issueGroups as $issueGroup) {
@@ -66,5 +69,15 @@ class ChangelogGenerator
         return count(array_filter($issues, function (Issue $issue) : bool {
             return $issue->isPullRequest();
         }));
+    }
+
+    /**
+     * @param Issue[] $issues
+     */
+    private function getNumberOfContributors(array $issues) : int
+    {
+        return count(array_unique(array_map(function (Issue $issue) : string {
+            return $issue->getUser();
+        }, $issues)));
     }
 }
